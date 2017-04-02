716948824d6f8c27858e6d00dc7b50e007fc2fab
Brandon Villa <bornofos@gmail.com>
2017-04-02 17:37:31 -0500

    master layout created

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,9 +7,16 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>MyFitnessProgram</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <!-- Custom styles for this template -->
+  <link href="css/carousel.css" rel="stylesheet">
+
 </head>
 
 <body>
+  @include('layouts.nav')
+
+  @include('layouts.carousel')
+  @include('layouts.heading')
 
   @yield('content')
 
