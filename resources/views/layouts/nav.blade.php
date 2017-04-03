99456422d397dc6270c3f49e60cd401fd1e97559
Brandon Villa <bornofos@gmail.com>
2017-04-03 01:10:13 -0500

    Login button created

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -19,8 +19,13 @@
       </li>
     </ul>
     <form class="form-inline mt-2 mt-md-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <input class="form-control mr-sm-2" type="text" placeholder="Find your plan...">
       <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
     </form>
+    <ul class="space nav navbar-nav navbar-right">
+      <li class="navbar-item">
+        <button class="nav-link btn btn-outline-primary" href="#">Log in</button>
+      </li>
+    </ul>
   </div>
 </nav>
