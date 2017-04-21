1369b28ee7cdecf592eaa30cce920699291d4516
marcos8896 <elyomarcos@gmail.com>
2017-04-21 14:01:51 -0600

    The display of the posts way has been enhanced

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,11 +2,10 @@
 
 @section('content')
 
-  <div class="col-sm-8 blog-main">
+  <div class="row">
 
     @foreach ($posts as $post)
         @include('posts.post')
-        <hr>
     @endforeach
 
     {{-- <nav class="blog-pagination">
--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,21 +1,43 @@
-<div class="blog-post">
-
+<div class="col-lg-4">
+  <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
   <h2 class="blog-post-title">
-
     <a href="/posts/{{ $post->id }}">
 
       {{ $post->title }}
 
     </a>
+    <hr>
+    <p class="h6">
+      {{ $post->user->name }} on
+      {{ $post->updated_at->toFormattedDateString() }}
+
+    </p>
+    <hr>
+    {{-- {{ $post->body }} --}}
 
   </h2>
+  <p><a class="btn btn-secondary" href="/posts/{{ $post->id }}" role="button">View details &raquo;</a></p>
+</div><!-- /.col-lg-4 -->
+{{--
 
-  <p class="blog-post-meta">
-    {{ $post->user->name }} on
-    {{ $post->updated_at->toFormattedDateString() }}
 
-  </p>
 
-  {{ $post->body }}
 
-</div><!-- /.blog-post -->
+
+<h2 class="blog-post-title">
+
+  <a href="/posts/{{ $post->id }}">
+
+    {{ $post->title }}
+
+  </a>
+
+</h2>
+
+<p class="blog-post-meta">
+  {{ $post->user->name }} on
+  {{ $post->updated_at->toFormattedDateString() }}
+
+</p>
+
+{{ $post->body }} --}}
