9605f5bdbe0d4db6f915e1ccc4549980b3c22845
marcos8896 <elyomarcos@gmail.com>
2017-04-23 01:25:38 -0600

    UserController and UserTable added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Image;
+
+class UserController extends Controller
+{
+
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
+  public function profile()
+  {
+    $user = Auth::user();
+    return view('users.profile', compact('user'));
+  }
+
+  public function updateAvatar()
+  {
+    if (request()->hasFile('avatar')) {
+      $avatar = request()->file('avatar');
+      $filename = time() . '.' . $avatar->getClientOriginalExtension();
+      Image::make($avatar)->resize(300, null, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+      })->save( public_path('/uploads/avatars/') . $filename);
+
+      $user = Auth::user();
+      $user->avatar = $filename;
+      $user->save();
+
+      return view('users.profile', compact('user'));
+    }
+  }
+}
