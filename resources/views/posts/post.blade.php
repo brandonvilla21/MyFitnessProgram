648375be3c87eb5d9047df36c98eab0fe6a414b4
marcos8896 <elyomarcos@gmail.com>
2017-04-27 23:19:52 -0500

    The way that posts are displayed on the homepage was improved

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,43 +1,13 @@
-<div class="col-lg-4 container">
-  <img class="img-show img-responsive" src="/uploads/posts/{{$post->image }}" alt="Generic placeholder image" width="400">
-  <h2 class="blog-post-title">
-    <a href="/posts/{{ $post->id }}">
-
-      {{ $post->title }}
-
-    </a>
-    <hr>
-    <p class="h6">
-      {{ $post->user->name }} on
-      {{ $post->updated_at->toFormattedDateString() }}
-
-    </p>
-    <hr>
-    {{-- {{ $post->body }} --}}
-
-  </h2>
-  <p><a class="btn btn-secondary" href="/posts/{{ $post->id }}" role="button">View details &raquo;</a></p>
-</div><!-- /.col-lg-4 -->
-{{--
-
-
-
-
-
-<h2 class="blog-post-title">
-
-  <a href="/posts/{{ $post->id }}">
-
-    {{ $post->title }}
-
-  </a>
-
-</h2>
-
-<p class="blog-post-meta">
-  {{ $post->user->name }} on
-  {{ $post->updated_at->toFormattedDateString() }}
-
-</p>
-
-{{ $post->body }} --}}
+<div class="col-lg-5 card card-margin">
+  <div class="crop">
+    <img class="card-img-top img-responsive" src="/uploads/posts/{{$post->image }}" alt="{{$post->title }}">
+  </div>
+  <span class="glyphicon glyphicon-time"></span><h6 class="small comment-meta text-center">Posted by<a href="#"> {{ $post->user->name }}</a> | {{ $post->created_at->diffForHumans() }}</h6>
+  <div class="card card-outline-secondary mb-3 text-center">
+    <div class="text-center">
+      <h4 class="card-header">{{$post->title }}</h4>
+      <p class="text-justify">{{ $preview = substr($post->body, 0, 100) . '...'}}</p>
+      <a href="/posts/{{ $post->id }}"class="btn btn-success btn-space-top">See post</a>
+    </div>
+  </div>
+</div>
